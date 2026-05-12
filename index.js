1a0272500a005a41d1ea361cfc598120b7f98927
treywwre-rgb <treyw.wre@gmail.com>
2026-05-12 10:11:38 -0500

    Working on modal__contact

---
+++ index.js
@@ -0,0 +1,33 @@
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+        .sendForm(
+            'contact_service',
+            'template_7b9yi1u',
+            event.target,
+            'MkR-BwDxaMGsLddrN'
+        ).then(() =>{
+           loading.classList.remove("modal__overlay--visible");
+           success.classList += " modal__overlay--visible";
+        }).catch(() => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "The email service is temporarily unavailable. Please contact me directly on"
+            );
+        })
+}
+
+let isModalOpen = false;
+function toggleModal() {
+    document.body.classList += " modal--open";
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+}
+    isModalOpen = true;
+    document.body.classList += " modal--open";
+}
+
